0ff3a9f15096b77ca62dd2eea9d7ff7f38ee77e5
Quocchinh123 <[200~dohoangquocchinh1200@gmail.com>
2024-08-25 20:19:38 +0700

    init project

---
+++ src/main/java/com/haui/shopthoitranghades/Dto/Constants.java
@@ -0,0 +1,7 @@
+package com.haui.shopthoitranghades.Dto;
+
+public interface Constants {
+	
+	public String UPLOAD_FILE_ROOT = "D:/clone/Website-thoi-trang/uploads/hahaha";
+	public String UPLOAD_FILE_ROOT1 = "D:/clone/Website-thoi-trang/uploads/";
+}
